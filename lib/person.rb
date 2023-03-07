a09061f93c9f8e93e24145ff2668ad96120651f3
Daniel Gitau <gitaud959@gmail.com>
2023-03-07 15:22:10 +0300

    Done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
-class Person
+class Personclass Person
+    def name 
+        @name
+    end
+
+    def name=(name)
+        @name=name
+    end
+
+    def job
+        @job
+    end
+
+    def job=(job)
+        @job = job
+    end
+end
+
 
 end
